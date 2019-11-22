db77279c40595e9e092fd1c4a0ebc48c6f2ae76f
Tomas Tobar <codetombomb@gmail.com>
2019-11-22 18:24:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -18,9 +18,13 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-# 3) 
+# 3) Define a Method With an Optional Argument
 
-
+def introduction_with_language_optional (name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+  
+  
 
 
 
